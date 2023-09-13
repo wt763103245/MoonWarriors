7b79aa4c1e699a292fe2cd967ac741a3b2c26a58
萌新王 <wt763103245@qq.com>
2023-09-13 17:07:22 +0800

    save

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,4 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    MainScene_json : "res/MainScene.json"
-    ,
     CloseNormal_png : "res/CloseNormal.png",
     CloseSelected_png : "res/CloseSelected.png",
 //shared
